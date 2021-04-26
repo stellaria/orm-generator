4f5410ad0649cc080a73bafcb287a19431dc5e37
stellaria <xpwanghy@hotmail.com>
2021-04-26 15:21:55 +0800

    搭建框架

---
+++ frontend/orm-generator/src/components/Hello.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>输入包名以开始</h1>
+    <el-input v-model="input" placeholder="请输入包名,精确到项目名"></el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: ''
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1{
+  height: 50px;
+  line-height: 50px;
+  margin-bottom: 200px;
+}
+.hello {
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+}
+</style>
